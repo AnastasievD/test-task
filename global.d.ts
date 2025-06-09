86b3d42cc6343d53c0e04baf61a3091f2835e137
AnastasievD <anastasievdima@gmail.com>
2025-06-09 15:09:53 +0300

    chore: add some libs and fix focfig

---
+++ global.d.ts
@@ -0,0 +1,10 @@
+declare module "react-icons/fa" {
+    import { FC, SVGProps } from "react"
+    const FaSearch: FC<SVGProps<SVGSVGElement>>
+    const FaCheck: FC<SVGProps<SVGSVGElement>>
+    const FaCog: FC<SVGProps<SVGSVGElement>>
+    const FaChevronDown: FC<SVGProps<SVGSVGElement>>
+    const FaMars: FC<SVGProps<SVGSVGElement>>,
+    const FaVenus: FC<SVGProps<SVGSVGElement>>
+    export { FaSearch, FaCheck, FaCog, FaChevronDown, FaVenus, FaMars }
+}
