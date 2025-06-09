e5242394f85ff7b38cce69f1330f7b8f707f96f4
AnastasievD <anastasievdima@gmail.com>
2025-06-09 15:10:58 +0300

    feat: table component

---
+++ src/components/sharedComponents/Table.tsx
@@ -0,0 +1,53 @@
+import * as React from "react";
+import classNames from 'classnames';
+
+const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
+  ({ className, ...props }, ref) => (
+    <div className="relative w-full">
+      <table
+        ref={ref}
+        className={classNames("w-full caption-bottom text-sm", className)}
+        {...props}
+      />
+    </div>
+  )
+);
+Table.displayName = "Table";
+
+const TableHeader = React.forwardRef<
+  HTMLTableSectionElement,
+  React.HTMLAttributes<HTMLTableSectionElement>
+>(({ className, ...props }, ref) => (
+  <thead ref={ref} className={classNames("[&_tr]:border-b", className)} {...props} />
+));
+TableHeader.displayName = "TableHeader";
+
+const TableBody = React.forwardRef<
+  HTMLTableSectionElement,
+  React.HTMLAttributes<HTMLTableSectionElement>
+>(({ className, ...props }, ref) => (
+  <tbody className={classNames("[&_tr:last-child]:border-0 overflow-auto", className)} ref={ref} {...props} />
+));
+TableBody.displayName = "TableBody";
+
+const TableRow = React.forwardRef<
+  HTMLTableRowElement,
+  React.HTMLAttributes<HTMLTableRowElement>
+>(({ className, ...props }, ref) => (
+  <tr
+    ref={ref}
+    className={classNames("border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted", className)}
+    {...props}
+  />
+));
+TableRow.displayName = "TableRow";
+
+const TableCell = React.forwardRef<
+  HTMLTableCellElement,
+  React.TdHTMLAttributes<HTMLTableCellElement>
+>(({ className, ...props }, ref) => (
+  <td ref={ref} className={classNames("p-4 align-middle", className)} {...props} />
+));
+TableCell.displayName = "TableCell";
+
+export { Table, TableHeader, TableBody, TableRow, TableCell };
